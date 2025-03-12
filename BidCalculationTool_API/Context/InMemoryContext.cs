1ad444138f00ee60ab55983c546ec523eb86f4db
Juan M. Lauredo <jpotterh@gmail.com>
2025-03-12 18:32:06 -0400

    fix typo

--- BidCalculationTool_API/Context/InMemoryContext.cs
+++ BidCalculationTool_API/Context/InMemoryContext.cs
@@ -5,8 +5,8 @@ namespace BidCalculationTool_API.Context
 {
     /*
      * InMemory might be the worst option to go with for testing purposes
-     * Chosen based on the implicity of the data storage requirements and
-     * as the less coniguration requiring option.
+     * Chosen based on the simplicity of the data storage requirements and
+     * as the less configuration requiring option.
      * 
      * Once the project gets green light, DB context structure is already
      * in place and it's just a matter of replacing current context by a
